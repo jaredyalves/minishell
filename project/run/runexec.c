f23bc22c2d27882f0f5b2353ca1766d995c864ac
jcapistr <jcapistr@student.42porto.com>
2024-05-16 10:48:31 +0100

    feat: search command in path

--- project/run/runexec.c
+++ project/run/runexec.c
@@ -1,17 +1,42 @@
 #include "../lexer/lexer.h"
-#include "../minishell.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+// FIXME: Can't use strcpy(), replace with a ft function
+// FIXME: Can't use strcat(), replace with a ft function
+// FIXME: Can't use strtok(), replace with a ft function
+void	search_in_path(char *program, char **argv, char **envp)
+{
+	char	*path;
+	char	*token;
+	char	executable[1024];
+
+	path = getenv("PATH");
+	token = strtok(path, ":");
+	while (token)
+	{
+		strcpy(executable, token);
+		strcat(executable, "/");
+		strcat(executable, program);
+		if (access(executable, X_OK) == 0)
+			execve(executable, argv, envp);
+		token = strtok(0, ":");
+	}
+}
 
 // FIXME: Can't use fprintf(), replace with a ft function
 void	runexec(t_execcmd *ecmd)
 {
 	if (ecmd->argv[0] == 0)
-		panic("minishell: runexec error");
-	execve(ecmd->argv[0], ecmd->argv, 0);
+		return ;
+	if (ecmd->argv[0][0] == '/' || (ecmd->argv[0][0] == '.'
+			&& ecmd->argv[0][1] == '/'))
+		execve(ecmd->argv[0], ecmd->argv, 0);
+	else
+		search_in_path(ecmd->argv[0], ecmd->argv, 0);
 	fprintf(stderr, "minishell: %s: command not found\n", ecmd->argv[0]);
-	exit(EXIT_FAILURE);
 }
 
 // vim: ts=4 sts=4 sw=4 noet
