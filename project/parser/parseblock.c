be8e83702dc87cbd19c6cd1a41fa8c629dac69e5
jcapistr <jcapistr@student.42porto.com>
2024-05-17 15:46:44 +0100

    fix: fix more memory leaks

--- project/parser/parseblock.c
+++ project/parser/parseblock.c
@@ -1,6 +1,7 @@
 #include "../lexer/lexer.h"
 #include "../minishell.h"
 #include "parser.h"
+#include "run/run.h"
 
 t_cmd	*parseblock(char **ps, char *es)
 {
@@ -11,7 +12,10 @@ t_cmd	*parseblock(char **ps, char *es)
 	get_token(ps, es, 0, 0);
 	cmd = parseline(ps, es);
 	if (!peek(ps, es, ")"))
+	{
+		freecmd(cmd);
 		panic("minishell: missing )");
+	}
 	get_token(ps, es, 0, 0);
 	cmd = parseredir(cmd, ps, es);
 	return (cmd);
