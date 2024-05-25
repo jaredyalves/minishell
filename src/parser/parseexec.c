dbc7f37493904debc5e9a75673a755c494cd5bef
jcapistr <jcapistr@student.42porto.com>
2024-05-25 09:42:09 +0100

    refactor: break parse execute into two functions

--- src/parser/parseexec.c
+++ src/parser/parseexec.c
@@ -1,30 +1,38 @@
 #include "minishell.h"
 
+int	parse_arguments(t_execcmd *e_command, char **ps, char *es)
+{
+	char	*q;
+	char	*eq;
+	t_token	token;
+
+	token = get_token(ps, es, &q, &eq);
+	if (token == TOKEN_NULL)
+		return (1);
+	if (token != TOKEN_NO_SPECIAL)
+		panic("syntax", (t_cmd *)e_command);
+	if (e_command->argc >= ARG_MAX)
+		panic("too many arguments", (t_cmd *)e_command);
+	e_command->argv[e_command->argc] = q;
+	e_command->eargv[e_command->argc] = eq;
+	e_command->argc++;
+	return (0);
+}
+
 t_cmd	*parseexec(char **ps, char *es)
 {
-	char		*q;
-	char		*eq;
-	t_token		 token;
-	t_execcmd	*cmd;
-	t_cmd		*ret;
+	t_cmd		*command;
+	t_execcmd	*e_command;
 
 	if (peek(ps, es, "("))
 		return (parseblock(ps, es));
-	ret = execute_command();
-	cmd = (t_execcmd *)ret;
+	command = execute_command();
+	e_command = (t_execcmd *)command;
 	while (!peek(ps, es, SYMBOLS))
 	{
-		token = get_token(ps, es, &q, &eq);
-		if (token == TOKEN_NULL)
+		if (parse_arguments(e_command, ps, es))
 			break ;
-		if (token != TOKEN_NO_SPECIAL)
-			panic("syntax", ret);
-		if (cmd->argc >= ARG_MAX)
-			panic("too many args", ret);
-		cmd->argv[cmd->argc] = q;
-		cmd->eargv[cmd->argc] = eq;
-		cmd->argc++;
-		ret = parseredir(ret, ps, es);
+		command = parseredir(command, ps, es);
 	}
-	return (ret);
+	return (command);
 }
