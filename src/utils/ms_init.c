69de8907d7effd0cbe6983ea5cfd4cadffdd3da6
jcapistr <jcapistr@student.42porto.com>
2024-06-06 11:23:42 +0100

    refactor: improve ms_init error handling and readability

--- src/utils/ms_init.c
+++ src/utils/ms_init.c
@@ -1,17 +1,26 @@
+#include <stdlib.h>
+
+#include "ft.h"
 #include "minishell.h"
 
 t_ms	*ms_init(const int argc, const char *argv[], char *envp[])
 {
 	t_ms	*ms;
-	char	**p;
+	size_t	envp_count;
+	size_t	i;
 
 	(void)argc;
 	(void)argv;
-	ms = (t_ms *)malloc(sizeof(*ms));
-	ft_memset(ms, 0, sizeof(*ms));
-	p = ms->env;
-	while (*envp)
-		*p++ = *envp++;
+	ms = ft_calloc(1, sizeof(*ms));
+	if (ms == NULL)
+		return (NULL);
+	envp_count = 0;
+	while (envp[envp_count] != NULL)
+		envp_count++;
+	i = -1;
+	while (++i < envp_count)
+		ms->env[i] = envp[i];
+	ms->env[i] = NULL;
 	setup_signal_handlers();
 	return (ms);
 }
