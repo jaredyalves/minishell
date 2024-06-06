eac73f3a75f2d2ea1b6d479849d41045ee3a95ee
jcapistr <jcapistr@student.42porto.com>
2024-06-06 11:50:39 +0100

    refactor: improve main for error handling and performance

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <sys/wait.h>
+
 #include "minishell.h"
 
 int	main(const int argc, const char *argv[], char *envp[])
@@ -5,18 +8,22 @@ int	main(const int argc, const char *argv[], char *envp[])
 	t_ms	*ms;
 
 	ms = ms_init(argc, argv, envp);
+	if (ms == NULL)
+		return (1);
 	while (1)
 	{
 		ms->str = get_str(ms->str);
 		if (ms->str == NULL)
 			break ;
 		ms->cmd = parse(ms->str);
-		if (ms->cmd && fork1() == 0)
+		if (ms->cmd == NULL)
+			continue ;
+		if (fork1() == 0)
 		{
 			ms->exit_status = execute(ms, ms->cmd);
 			exit(ms_exit(&ms));
 		}
-		waitpid(0, &ms->wait_status, 0);
+		waitpid(-1, &ms->wait_status, 0);
 		free_cmd(&ms->cmd);
 		free_str(&ms->str);
 	}
