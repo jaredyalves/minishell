26ba86f3e819b223292fe5fed6c4e4e8b0320519
jcapistr <jcapistr@student.42porto.com>
2024-06-22 21:52:19 +0100

    feat: improve the exit builtin function

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,54 +6,52 @@
 /*   By: joamonte <joamonte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 08:39:06 by joamonte          #+#    #+#             */
-/*   Updated: 2024/06/21 16:21:01 by joamonte         ###   ########.fr       */
+/*   Updated: 2024/06/22 21:41:53 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-#include <unistd.h>
-#include <stdio.h>
-
-static int	ft_exit_continue(char**args)
+static int	is_valid(char *arg)
 {
 	int	i;
 
 	i = 0;
-	while (args[1][i])
+	if (arg[i] == '-')
+		i++;
+	if (!arg[i])
+		return (0);
+	while (arg[i])
 	{
-		if (args[1][i] == '-')
-			i++;
-		if ((args[1][i]) < '0' || (args[1][i]) > '9')
-		{
-			printf("exit Error: numeric argument required\n");
-			printf("Goodbye...See you soon!\n");
-			sh_deinit(2);
-			return (2);
-		}
+		if (!ft_isdigit(arg[i]))
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
 int	ft_exit(char **args)
 {
+	char	status;
+
+	ft_putstr_fd("exit\n", STDOUT_FILENO);
+	status = (char)get_sh()->exit_status;
 	if (args[1])
 	{
-		if(ft_exit_continue(args) == 2)
-			return (2);
+		if (!is_valid(args[1]))
+		{
+			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+			ft_putstr_fd(args[1], STDERR_FILENO);
+			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+			sh_deinit(2);
+		}
+		status = (char)ft_atoi(args[1]);
 	}
 	if (args[2])
 	{
-		printf("exit Error: too many arguments\n");
-		return(2);
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd("too many arguments\n", STDERR_FILENO);
+		return (1);
 	}
-	printf("Goodbye...See you soon!\n");
-	if (args[1])
-	{
-		sh_deinit(ft_atoi(args[1]));
-		return (0);
-	}
-	sh_deinit(0);
-	return (0);
+	sh_deinit(status);
 }
