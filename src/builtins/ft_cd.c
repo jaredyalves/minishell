999737c38bcb744d1159924e6a771788d4c0f5d5
jcapistr <jcapistr@student.42porto.com>
2024-07-26 19:45:24 +0100

    fix(builtin): improve cd

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -10,85 +10,89 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "minishell.h"
-#include <stdio.h>
+
+#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 static int	check_args(char **args)
 {
-	if (args[2])
+	int	count;
+
+	if (args == NULL)
 	{
-		printf("cd Error : Wrong number of arguments\n");
-		return (-1);
+		ft_putstr_fd("minishell: cd: something went wrong\n", STDERR_FILENO);
+		return (0);
 	}
-	return (0);
+	count = 0;
+	while (args[count])
+		count++;
+	if (count > 2)
+	{
+		ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
+		return (0);
+	}
+	return (1);
 }
 
-static void	set_pwd(int flag)
+static void	update_env_vars(char *old_pwd)
 {
-	char	directory[4097];
-	char	*pwd[3];
+	char	pwd[PATH_MAX];
+	char	*cmd[3];
 
-	getcwd(directory, sizeof(directory));
-	pwd[0] = ft_strdup("export");
-	if (flag == 2)
-		pwd[1] = ft_strjoin(ft_strdup("OLDPWD="), directory);
-	else if (flag == 1)
-		pwd[1] = ft_strjoin(ft_strdup("PWD="), directory);
-	pwd[2] = NULL;
-	ft_export(pwd);
-	free(pwd[0]);
-	free(pwd[1]);
+	getcwd(pwd, sizeof(pwd));
+	cmd[0] = "export";
+	cmd[2] = NULL;
+	cmd[1] = ft_strjoin("OLDPWD=", old_pwd);
+	ft_export(cmd);
+	free(cmd[1]);
+	cmd[1] = ft_strjoin("PWD=", pwd);
+	ft_export(cmd);
+	free(cmd[1]);
 }
 
-static int	change_to_home(void)
+static int	change_directory(char *path, int print_path)
 {
-	char	*homedir;
+	char	pwd[PATH_MAX];
 
-	homedir = ft_getenv("HOME");
-	if (!homedir)
+	getcwd(pwd, sizeof(pwd));
+	if (chdir(path) == -1)
 	{
-		printf("cd ERROR: HOME not set\n");
-		return (free(homedir), 1);
+		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+		ft_putstr_fd(path, STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+		ft_putendl_fd(strerror(errno), STDERR_FILENO);
+		return (free(path), 1);
 	}
-	chdir(homedir);
-	set_pwd(1);
-	return (free(homedir), 0);
-}
-
-static int	go_back(char *old_pwd)
-{
-	if (!old_pwd)
-	{
-		printf("minishell: cd ERROR: OLDPWD not set\n");
-		return (free(old_pwd), 1);
-	}
-	chdir(old_pwd);
-	return (0);
+	update_env_vars(pwd);
+	if (print_path)
+		ft_putendl_fd(path, STDOUT_FILENO);
+	return (free(path), 0);
 }
 
 int	ft_cd(char **args)
 {
-	char	*old_pwd;
-	int		exit;
+	char	*path;
 
-	old_pwd = ft_getenv("OLDPWD");
-	if (check_args(args) == -1)
-		return (free(old_pwd), -1);
-	set_pwd(2);
-	if (!args[1] || args[1] == NULL || (args[1][0] == '~' && !args[1][1]))
-		return (free(old_pwd), change_to_home());
-	if ((args[1][0] == '-' && !args[1][1]))
+	if (!check_args(args))
+		return (1);
+	if (args[1] == NULL || (args[1] && ft_strncmp(args[1], "~", 2) == 0))
 	{
-		exit = go_back(old_pwd);
-		set_pwd(1);
-		return (free(old_pwd), exit);
+		path = ft_getenv("HOME");
+		if (path == NULL)
+			return (ft_putstr_fd("minishell: cd: HOME not set\n", 2), 1);
+		return (change_directory(path, 0));
+	}
+	if (args[1] && ft_strncmp(args[1], "-", 2) == 0)
+	{
+		path = ft_getenv("OLDPWD");
+		if (path == NULL)
+			return (ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2), 1);
+		return (change_directory(path, 1));
 	}
 	if (args[1])
-	{
-		if (chdir(args[1]))
-			perror("cd");
-	}
-	set_pwd(1);
-	return (free(old_pwd), 0);
+		return (change_directory(ft_strdup(args[1]), 0));
+	return (1);
 }
